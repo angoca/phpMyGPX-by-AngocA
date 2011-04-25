8a9ea72eb2a01655d9653c396633e98a8975eb5f
Andres Gomez Casanova <angoca@yahoo.com>
2011-04-25 11:52:25 -0500

    All new functions
    Functions to retrieve photos with empty body
    Spaces instead of tabs

--- phpmygpx/photoGeoRef.php
+++ phpmygpx/photoGeoRef.php
@@ -23,62 +23,132 @@ if($cfg['show_exec_time'])
 $startTime = microtime_float();
 
 if($DEBUG) {
-	foreach($_POST as $akey => $val)
-	out("<b>$akey</b> = $val", "OUT_DEBUG");
+    foreach($_POST as $akey => $val)
+    out("<b>$akey</b> = $val", "OUT_DEBUG");
 }
 
-$task 	= getUrlParam('HTTP_GET', 'STRING', 'task');
-$f_x 	= getUrlParam('HTTP_GET', 'FLOAT', 'x') * 1000000;
-$f_y 	= getUrlParam('HTTP_GET', 'FLOAT', 'y') * 1000000;
+$task = getUrlParam('HTTP_GET', 'STRING', 'task');
+$f_x = getUrlParam('HTTP_GET', 'FLOAT', 'x') * 1000000;
+$f_y = getUrlParam('HTTP_GET', 'FLOAT', 'y') * 1000000;
+$f_limit = getUrlParam('HTTP_GET', 'FLOAT', 'limit');
+$f_img_dir = getUrlParam('HTTP_GET', 'FLOAT', 'img_dir');
+$f_img_delta = getUrlParam('HTTP_GET', 'FLOAT', 'img_delta');
+$f_pos_dir = getUrlParam('HTTP_GET', 'FLOAT', 'pos_dir');
+$f_pos_delta = getUrlParam('HTTP_GET', 'FLOAT', 'pos_delta');
 
 // Connects to database
 $link = db_connect_h($cfg['db_host'], $cfg['db_name'], $cfg['db_user'],
-		$cfg['db_password']);
+        $cfg['db_password']);
 
 switch ($task) {
-	case 'getCloserPhoto':
-		getCloserPhoto($f_x, $f_y);
-		break;
+    case 'getClosestPhotos':
+        getClosestPhotos($f_x, $f_y, $f_limit);
+        break;
 
-	default:
-		getData();
-		break;
+    case 'getClosestPhoto':
+        getClosestPhoto($f_x, $f_y);
+        break;
+
+    case 'getClosestPhotosWithOrientation':
+        getClosestPhotosWithOrientation($f_x, $f_y, $f_img_dir, $f_img_delta, $f_limit);
+        break;
+
+    case 'getClosestPhotoWithOrientation':
+        getClosestPhotoWithOrientation($f_x, $f_y, $f_img_dir, $f_img_delta);
+        break;
+
+    case 'getClosestPhotosOverDirection':
+        getClosestPhotosOverDirection($f_x, $f_y, $f_pos_dir, $f_pos_delta, $f_limit);
+        break;
+
+    case 'getClosestPhotoOverDirection':
+        getClosestPhotoOverDirection($f_x, $f_y, $f_pos_dir, $f_pos_delta);
+        break;
+
+    case 'getClosestPhotosWithOrientationOverDirection':
+        getClosestPhotosWithOrientationOverDirection($f_x, $f_y, $f_pos_dir, $f_pos_delta, $f_img_dir, $f_img_delta, $f_limit);
+        break;
+
+    case 'getClosestPhotoWithOrientationOverDirection':
+        getClosestPhotoWithOrientationOverDirection($f_x, $f_y, $f_pos_dir, $f_pos_delta, $f_img_dir, $f_img_delta);
+        break;
+
+    default:
+        getData();
+        break;
 }
 
-function getCloserPhoto($x, $y) {
-	global $DEBUG, $cfg, $dbqueries;
-	$query = $dbqueries['closerPhoto_1'].$x.$dbqueries['closerPhoto_2'].$y
-			.$dbqueries['closerPhoto_3'];
-	if($DEBUG)	out($query, 'OUT_DEBUG');
-	$result = db_query($query);
-	if(mysql_num_rows($result)) {
-		$row = mysql_fetch_array($result, MYSQL_ASSOC);
-		// Writes the description in JSON format.
-		$ret = "{";
-		$ret .= "  \"photo\": {";
-		$ret .= "    \"id\": \"".$row['id'].'", ';
-		$ret .= "    \"altitude\": \"".$row['altitude'].'", ';
-		$ret .= "    \"latitude\": \"".($row['latitude']/1000000).'", ';
-		$ret .= "    \"longitude\": \"".($row['longitude']/1000000).'", ';
-		$ret .= "    \"timestamp\": \"".$row['timestamp'].'", ';
-		$ret .= "    \"image_dir\": \"".$row['image_dir'].'", ';
-		$ret .= "    \"speed\": \"".$row['speed'].'", ';
-		$ret .= "    \"move_dir\": \"".$row['move_dir'].'", ';
-		$ret .= "    \"size\": \"".$row['size'].'", ';
-		$ret .= "    \"file\": \"".$row['file'].'", ';
-		$ret .= "  }";
-		$ret .= "}";
-		echo $ret;
-		
-		// Writes an hyperlink tothe photo.
-		echo "<p><a href='${cfg['photo_images_dir']}${row['file']}'>";
-		echo "<img src='${cfg['photo_thumbs_dir']}${cfg['thumbs_prefix']}${row['file']}' hspace=5 /></a></p>";
-	}
+function getClosestPhotos($x, $y, $limit) {
+    global $DEBUG, $cfg, $dbqueries;
+    echo "{\"Note\": \"To be done ".$x."-".$y."-".$limit."\"}";
+}
+
+function getClosestPhoto($x, $y) {
+    global $DEBUG, $cfg, $dbqueries;
+    $query = $dbqueries['closestPhoto_1'].$x.$dbqueries['closestPhoto_2'].$y
+            .$dbqueries['closestPhoto_3'];
+    if($DEBUG)	out($query, 'OUT_DEBUG');
+    $result = db_query($query);
+    if(mysql_num_rows($result)) {
+        $row = mysql_fetch_array($result, MYSQL_ASSOC);
+        // Writes the description in JSON format.
+        $ret = "{";
+        $ret .= "  \"photo\": {";
+        $ret .= "    \"id\": \"".$row['id'].'", ';
+        $ret .= "    \"altitude\": \"".$row['altitude'].'", ';
+        $ret .= "    \"latitude\": \"".($row['latitude']/1000000).'", ';
+        $ret .= "    \"longitude\": \"".($row['longitude']/1000000).'", ';
+        $ret .= "    \"timestamp\": \"".$row['timestamp'].'", ';
+        $ret .= "    \"image_dir\": \"".$row['image_dir'].'", ';
+        $ret .= "    \"speed\": \"".$row['speed'].'", ';
+        $ret .= "    \"move_dir\": \"".$row['move_dir'].'", ';
+        $ret .= "    \"size\": \"".$row['size'].'", ';
+        $ret .= "    \"file\": \"".$row['file'].'", ';
+        $ret .= "  }";
+        $ret .= "}";
+        echo $ret;
+
+        // Writes an hyperlink tothe photo.
+        echo "<p><a href='${cfg['photo_images_dir']}${row['file']}'>";
+        echo "<img src='${cfg['photo_thumbs_dir']}${cfg['thumbs_prefix']}${row['file']}' hspace=5 /></a></p>";
+    } else {
+        echo "{}";
+    }
+}
+
+function getClosestPhotosWithOrientation($x, $y, $img_dir, $img_delta, $limit) {
+    global $DEBUG, $cfg, $dbqueries;
+    echo "{\"Note\": \"To be done ".$x."-".$y."-".$img_dir."-".$img_delta."-".$limit."\"}";
+}
+
+function getClosestPhotoWithOrientation($x, $y, $img_dir, $img_delta) {
+    global $DEBUG, $cfg, $dbqueries;
+    echo "{\"Note\": \"To be done ".$x."-".$y."-".$img_dir."-".$img_delta."\"}";
+}
+
+function getClosestPhotosOverDirection($x, $y, $pos_dir, $pos_delta, $limit) {
+    global $DEBUG, $cfg, $dbqueries;
+    echo "{\"Note\": \"To be done ".$x."-".$y."-".$pos_dir."-".$pos_delta."-".$limit."\"}";
+}
+
+function getClosestPhotoOverDirection($x, $y, $pos_dir, $pos_delta) {
+    global $DEBUG, $cfg, $dbqueries;
+    echo "{\"Note\": \"To be done ".$x."-".$y."-".$pos_dir."-".$pos_delta."\"}";
+}
+
+function getClosestPhotosWithOrientationOverDirection($x, $y, $pos_dir, $pos_delta, $img_dir, $img_delta, $limit) {
+    global $DEBUG, $cfg, $dbqueries;
+    echo "{\"Note\": \"To be done ".$x."-".$y."-".$pos_dir."-".$pos_delta."-".$img_dir."-".$img_delta."-".$limit."\"}";
+}
+
+function getClosestPhotoWithOrientationOverDirection($x, $y, $pos_dir, $pos_delta, $img_dir, $img_delta) {
+    global $DEBUG, $cfg, $dbqueries;
+    echo "{\"Note\": \"To be done ".$x."-".$y."-".$pos_dir."-".$pos_delta."-".$img_dir."-".$img_delta."\"}";
 }
 
 if($cfg['show_exec_time']) {
-	$endTime = microtime_float();
-	$exectime = round($endTime - $startTime, 4);
+    $endTime = microtime_float();
+    $exectime = round($endTime - $startTime, 4);
 }
 
 ?>
